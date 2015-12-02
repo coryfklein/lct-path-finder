200159ea4686f6140511220460e46b339415269e
Cory Klein <coryfklein@gmail.com>
2015-12-02 06:26:58 -0700

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "lct-path-finder"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by cory on 12/2/15.
+  */
+object Main {
+  def main(args: Array[String]) = println("Hi!")
+}
